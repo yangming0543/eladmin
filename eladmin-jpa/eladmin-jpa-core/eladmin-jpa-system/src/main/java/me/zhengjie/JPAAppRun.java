b4bdf882a1b580b2fa0e3d6410c347c1f47da3c8
yangming0543 <yangming0543@outlook.com>
2024-09-08 20:27:33 +0800

    first version

--- eladmin-jpa/eladmin-jpa-core/eladmin-jpa-system/src/main/java/me/zhengjie/JPAAppRun.java
+++ eladmin-jpa/eladmin-jpa-core/eladmin-jpa-system/src/main/java/me/zhengjie/JPAAppRun.java
@@ -39,10 +39,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
-public class AppRun {
+public class JPAAppRun {
 
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(AppRun.class);
+        SpringApplication springApplication = new SpringApplication(JPAAppRun.class);
         // 监控应用的PID，启动时可指定PID路径：--spring.pid.file=/home/eladmin/app.pid
         // 或者在 application.yml 添加文件路径，方便 kill，kill `cat /home/eladmin/app.pid`
         springApplication.addListeners(new ApplicationPidFileWriter());
