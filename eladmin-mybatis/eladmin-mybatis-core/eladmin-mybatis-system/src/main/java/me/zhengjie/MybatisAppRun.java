136ae7496f684d18ae22153684009ca6683f3002
yangming0543 <yangming0543@outlook.com>
2024-09-08 20:41:14 +0800

    first version

--- eladmin-mybatis/eladmin-mybatis-core/eladmin-mybatis-system/src/main/java/me/zhengjie/MybatisAppRun.java
+++ eladmin-mybatis/eladmin-mybatis-core/eladmin-mybatis-system/src/main/java/me/zhengjie/MybatisAppRun.java
@@ -37,10 +37,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(hidden = true)
 @SpringBootApplication
 @EnableTransactionManagement
-public class AppRun {
+public class MybatisAppRun {
 
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(AppRun.class);
+        SpringApplication springApplication = new SpringApplication(MybatisAppRun.class);
         // 监控应用的PID，启动时可指定PID路径：--spring.pid.file=/home/eladmin/app.pid
         // 或者在 application.yml 添加文件路径，方便 kill，kill `cat /home/eladmin/app.pid`
         springApplication.addListeners(new ApplicationPidFileWriter());
